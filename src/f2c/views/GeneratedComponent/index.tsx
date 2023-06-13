40412a302305cdd42e43ce99b437b15814ccb6f7
liangzhirong <liangzhirong@yy.com>
2023-06-13 13:38:58 +0800

    chore: up

--- src/f2c/views/GeneratedComponent/index.tsx
+++ src/f2c/views/GeneratedComponent/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import styles from "./index.module.scss";
 
-const index = () => {
-  return (
-    <div>
-      
+const GeneratedComponent = () => (
+  <div className={styles["f2c-7-l0Uqmv"]}>
+    <div className={styles["f2c-qTH26o1n"]}>
+      <div className={styles["f2c-NATnnnnV"]}>
+        <div className={styles["f2c-CDZhDUm-"]}> </div>
+        <div className={styles["f2c-Xk3DCCwS"]}> </div>
+      </div>
+      <img
+        src={require("./assets/ImageAsset2.png")}
+        alt="Image Asset 2"
+        className={styles["f2c-efmiLNxs"]}
+      />
+      <div className={styles["f2c-YHvF7dWZ"]}>
+        <div className={styles["f2c-qaj4k2Vw"]}> </div>
+        <div className={styles["f2c-wt1Gxefo"]}> </div>
+      </div>
     </div>
-  );
-};
+    <div className={styles["f2c-kh1668rJ"]}>
+      单次送出礼物名/礼物名达
+      <span className={styles["f2c-1-kh1668rJ"]}> 50W贡献值 </span>
+      可唤起活动飞机
+    </div>
+    <div className={styles["f2c-nkJ3nXtv"]}>
+      <div className={styles["f2c-MZJHt-ku"]}>
+        <img
+          src={require("./assets/ImageAsset3.png")}
+          alt="Image Asset 3"
+          className={styles["f2c-DRv3xtLa"]}
+        />
+      </div>
+      <div className={styles["f2c-nx8dYrFU"]}> </div>
+    </div>
+  </div>
+);
 
-export default index;
+export default GeneratedComponent;
